fa8d1db3962463154e4613a70bdb4aa548181ae1
Gurlev Andrey <Dusha2b63Gur@yandex.ru>
2016-10-01 15:07:08 +0800

    сделал редизайн страниц всех афиш

--- template/blockPageAfishaThree.php
+++ template/blockPageAfishaThree.php
@@ -2,7 +2,7 @@
 <section>
 	<?php 
     $db = new PDO("sqlite:admin/post3.db");
-  	$res = $db->query("SELECT * FROM afisha WHERE name_tipe = 'постоянные события';");
+  	$res = $db->query("SELECT * FROM afisha WHERE name_tipe = 'мероприятия';");
 	foreach ($res as $array) {
 		$block_img = $array['img'];
 		$block_zag = $array['zag'];
@@ -11,28 +11,26 @@
         $block_tel = $array['tel'];
         $block_money = $array['money'];
         $block_text = $array['tipe'];
-         echo("<div class=\"pageAfishaBlock\">");
-		 echo("<div class=\"imgAfishaBlock\">");
-		 echo("<img src=".$block_img." title=\"".$block_zag."\" alt=\"".$block_zag."\" />");
-		 echo("<p class=\"spravkaAfisha\">");
-		 echo("<a href=\"#\" class=\"celendar\"></a><span>".$block_time."</span><br>");
-		 echo("<a href=\"#\" class=\"situation\"></a><span>".$block_place."</span><br>");
-		 echo("<a href=\"#\" class=\"tel\"></a><span>".$block_tel."</span><br>");
-		 echo("<a href=\"#\" class=\"banknote\"></a><span>".$block_money."</span><br>");
-		 echo("</p>");
+        $block_tipe = $array['name_tipe'];
+		 echo("<div class=\"blog-card alt\">");
+	     echo("<div class=\"photo\" style=\"background: url(".$block_img.") center / cover no-repeat;\"></div>");
+	     echo("<ul class=\"details\">");
+		 echo("<li class=\"author\">".$block_tel."</li>");
+		 echo("<li class=\"date\">".$block_money."</li>");
+		 echo("<li class=\"author\"><a href=\"#\">".$block_place."</a></li>");
+		 echo("<li class=\"date\">".$block_time."</li>");
+		 echo("</ul>");
+		 echo("<div class=\"description\">");
+		 echo("<h1>".$block_zag."</h1>");
+		 echo("<h2>".$block_tipe."</h2>");
+		 echo("<p class=\"summary\">".$block_text."</p>");
+	     echo("</div>");
 		 echo("</div>");
-		 echo("<div class=\"contentAfishaBlock\">");
-		 echo("<p>".$block_zag."</p><hr>");
-		 echo("<p>".$block_text."</p>");
-		 echo("</div>");
-		 echo("</div>");
-		 }	
+		 }
 	?>
 </section>
 
-
-
-
+   
 
 
 
